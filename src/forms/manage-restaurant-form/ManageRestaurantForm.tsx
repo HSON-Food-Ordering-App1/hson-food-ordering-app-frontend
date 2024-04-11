2a8cd781fd893b39bd511133950ec3f8f4fbf242
sonlh02 <let02hb@gmail.com>
2024-04-11 16:19:41 +0700

    Created restaurant API request

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -37,10 +37,16 @@ const formSchema = z.object({
       price: z.coerce.number().min(1, "giá không được để trống"),
     })
   ),
-  imageFile: z.instanceof(File, {message: "hình ảnh không được để trống"}),
+  //imageUrl: z.string().optional(),
+  imageFile: z.instanceof(File, { message: "hình ảnh không được để trống" }),
+  //.optional(),
 });
+// .refine((data) => data.imageUrl || data.imageFile, {
+//   message: "Either image URL or image File must be provided",
+//   path: ["imageFile"],
+// });
 
-type restaurantFormData = z.infer<typeof formSchema>;
+type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
   onSave: (restaurantFormData: FormData) => void;
@@ -48,32 +54,62 @@ type Props = {
 };
 
 const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
-    const form = useForm<restaurantFormData>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            cuisines: [],
-            menuItems: [{ name: "", price: 0 }],
-        },
+  const form = useForm<RestaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+
+  const onSubmit = (formDataJson: RestaurantFormData) => {
+    const formData = new FormData();
+
+    formData.append("restaurantName", formDataJson.restaurantName);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+
+    formData.append(
+      "deliveryPrice",
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
     });
 
-    const onSubmit = (formDataJson: restaurantFormData) => {
+    formData.append(`imageFile`, formDataJson.imageFile);
 
-    }
+    onSave(formData);
+  };
 
-    return(
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
-                <DetailsSection/>
-                <Separator/>
-                <CuisinesSection/>
-                <Separator/>
-                <MenuSection/>
-                <Separator/>
-                <ImageSection/>
-                {isLoading ? <LoadingButton/> : <Button type="submit">Submit</Button>}
-            </form>
-        </Form>
-    )
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+        <Separator />
+        <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+      </form>
+    </Form>
+  );
 };
 
 export default ManageRestaurantForm;
