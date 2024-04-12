c801ce72df1f3f16ac85f7680b5b213c0d42bcf8
sonlh02 <let02hb@gmail.com>
2024-04-12 10:07:01 +0700

    Pre-populated restaurant form

--- src/forms/manage-restaurant-form/ImageSection.tsx
+++ src/forms/manage-restaurant-form/ImageSection.tsx
@@ -6,10 +6,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { useFormContext } from "react-hook-form";
 
 const ImageSection = () => {
-  const { control } = useFormContext();
+  const { control, watch } = useFormContext();
+
+  const existingImageUrl = watch("imageUrl");
 
   return (
     <div className="space-y-2">
@@ -22,7 +25,12 @@ const ImageSection = () => {
         </FormDescription>
       </div>
 
-      <div className="flex flex-col gap-8 w-[50%]">
+      <div className="flex flex-col gap-8 md:w-[50%]">
+        {existingImageUrl && (
+          <AspectRatio ratio={16/9}>
+            <img src={existingImageUrl} className="rounded-md object-cover h-full w-full"/>
+          </AspectRatio>
+        )}
         <FormField
           control={control}
           name="imageFile"
@@ -40,7 +48,7 @@ const ImageSection = () => {
                   }
                 />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
