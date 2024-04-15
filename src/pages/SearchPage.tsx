2f1b33ba3847759836224061f756fc461db6f802
sonlh02 <let02hb@gmail.com>
2024-04-15 14:57:36 +0700

    Added search API request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,22 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
 
-  return(
-    <span>Người dùng đã tìm kiếm {city}</span>
-  )
+  return (
+    <span>
+      Người dùng đã tìm kiếm {city}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 };
 
 export default SearchPage;
