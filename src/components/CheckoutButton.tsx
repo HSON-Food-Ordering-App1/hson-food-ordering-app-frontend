ae564a20ee626a5ef7454eb9bc26a2acb235a425
sonlh02 <let02hb@gmail.com>
2024-04-21 17:00:34 +0700

    Updated user profile form

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -3,15 +3,17 @@ import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
-import UserProfileForm, { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import UserProfileForm, {
+  UserFormData,
+} from "@/forms/user-profile-form/UserProfileForm";
 import { useGetMyUser } from "@/api/MyUserApi";
 
 type Props = {
   onCheckout: (userFormData: UserFormData) => void;
   disabled: boolean;
-}
+};
 
-const CheckoutButton = ({onCheckout, disabled}: Props) => {
+const CheckoutButton = ({ onCheckout, disabled }: Props) => {
   const {
     isAuthenticated,
     isLoading: isAuthLoading,
@@ -46,13 +48,17 @@ const CheckoutButton = ({onCheckout, disabled}: Props) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button disabled={disabled} className="bg-orange-500 flex-1">Đi đến thanh toán</Button>
+        <Button disabled={disabled} className="bg-orange-500 flex-1">
+          Đi đến thanh toán
+        </Button>
       </DialogTrigger>
       <DialogContent className="max-w-[425px] md:win-w-[700px] bg-gray-50">
         <UserProfileForm
           currentUser={currentUser}
           onSave={onCheckout}
           isLoading={isGetUserLoading}
+          title="Xác nhận chi tiết giao hàng"
+          buttonText="Tiếp tục thanh toán"
         />
       </DialogContent>
     </Dialog>
--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -30,9 +30,17 @@ type Props = {
   currentUser: User;
   onSave: (UserProfileData: UserFormData) => void;
   isLoading: boolean;
+  title?: string;
+  buttonText?: string;
 };
 
-const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+const UserProfileForm = ({
+  onSave,
+  isLoading,
+  currentUser,
+  title = "Thông tin người dùng",
+  buttonText = "Xác nhận",
+}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -49,7 +57,7 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
         className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className="text-2xl font-bold">Thông tin người dùng</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
           <FormDescription>
             Xem và thay đổi thông tin hồ sơ của bạn tại đây
           </FormDescription>
@@ -129,7 +137,7 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
           <LoadingButton />
         ) : (
           <Button type="submit" className="bg-orange-500">
-            Submit
+            {buttonText}
           </Button>
         )}
       </form>
